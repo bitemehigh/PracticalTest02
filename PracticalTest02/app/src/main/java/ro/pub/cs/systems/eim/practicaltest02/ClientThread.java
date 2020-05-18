b7dd5041447651f6ecc4520a95886611c2d295f6
bitemehigh <mihai.vasile2209@stud.acs.upb.ro>
2020-05-18 09:29:00 +0300

    app without 1 min updates

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/ClientThread.java
@@ -0,0 +1,81 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import ro.pub.cs.systems.eim.practicaltest02.Constants;
+import ro.pub.cs.systems.eim.practicaltest02.Utilities;
+
+public class ClientThread extends Thread {
+
+    private String address;
+    private int port;
+    private String currency;
+    private TextView currencyTextView;
+
+    private Socket socket;
+
+    public ClientThread(String address, int port, String currency, TextView currencyTextView) {
+        this.address = address;
+        this.port = port;
+        this.currency = currency;
+        this.currencyTextView = currencyTextView;
+    }
+
+    @Override
+    public void run() {
+        try {
+            socket = new Socket(address, port);
+
+            if (socket == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Could not create socket!");
+                return;
+            }
+
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[CLIENT THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+
+            printWriter.println(currency);
+            printWriter.flush();
+
+            String currencyInformation;
+
+            while ((currencyInformation = bufferedReader.readLine()) != null) {
+                final String finalizedInformation = currencyInformation;
+                currencyTextView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        currencyTextView.setText(finalizedInformation);
+                    }
+                });
+            }
+
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[CLIENT THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/CommunicationThread.java
@@ -0,0 +1,126 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.HashMap;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.util.EntityUtils;
+
+public class CommunicationThread extends Thread {
+
+    private ServerThread serverThread;
+    private Socket socket;
+
+    public CommunicationThread(ServerThread serverThread, Socket socket) {
+        this.serverThread = serverThread;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        if (socket == null) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] Socket is null!");
+            return;
+        }
+        try {
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+
+            Log.i(Constants.TAG, "[COMMUNICATION THREAD] Waiting for parameters from client (currency type!");
+
+            String currency = bufferedReader.readLine();
+
+            if (currency == null || currency.isEmpty()) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error receiving parameters from client (currency type!");
+                return;
+            }
+            HashMap<String, Double> data = serverThread.getData();
+
+            Double currencyInformation = null;
+            if (data.containsKey(currency)) {
+                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the cache...");
+                currencyInformation = data.get(currency);
+            } else {
+                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the webservice...");
+                HttpClient httpClient = new DefaultHttpClient();
+                String pageSourceCode = "";
+
+                HttpGet httpGet = new HttpGet(Constants.WEB_SERVICE_ADDRESS + currency + ".json");
+                HttpResponse httpGetResponse = httpClient.execute(httpGet);
+                HttpEntity httpGetEntity = httpGetResponse.getEntity();
+                if (httpGetEntity != null) {
+                    pageSourceCode = EntityUtils.toString(httpGetEntity);
+                }
+
+                if (pageSourceCode == null) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error getting the information from the webservice!");
+                    return;
+                } else
+                    Log.i(Constants.TAG, pageSourceCode);
+
+                JSONObject content = new JSONObject(pageSourceCode);
+
+                JSONObject bpi = content.getJSONObject("bpi");
+                JSONObject currency_info = bpi.getJSONObject(currency);
+                String rate_float = currency_info.getString("rate_float");
+
+                currencyInformation = Double.parseDouble(rate_float);
+
+                serverThread.setData(currency, currencyInformation);
+            }
+
+            if (currencyInformation == null) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Weather Forecast Information is null!");
+                return;
+            }
+
+            String result = currencyInformation.toString();
+            printWriter.println(result);
+            printWriter.flush();
+
+        } catch (IOException ioException) {
+
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+
+        } catch (JSONException jsonException) {
+
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + jsonException.getMessage());
+            if (Constants.DEBUG) {
+                jsonException.printStackTrace();
+            }
+
+        } finally {
+
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+
+        }
+    }
+}
